5f59f07e569c65c33f972d62db9a097db2a34b4e
Hongnan <hongnang.sph@gmail.com>
2021-04-24 14:17:34 +0800

    1

--- model.py
+++ model.py
@@ -40,7 +40,9 @@ class SHOPEE_HIRE_ME_MODEL(nn.Module):
         )  # fc_dim is the embeddings we want, which is proposed to be 512.
 
         self.ArcMargin = ArcModule(
-            in_features=self.embedding_size, out_features=self.num_classes, s=64, m=0.5
+            in_features=self.embedding_size,
+            out_features=self.num_classes,
+            **CONFIG["ArcFace"]
         )
 
     def forward(self, x, labels=None):
@@ -71,7 +73,9 @@ class SHOPEE_HIRE_ME_MODEL_IDKSHAPEERROR(nn.Module):
 
         self.in_features = self.backbone.head.fc.in_features
         self.margin = ArcModule(
-            in_features=self.embedding_size, out_features=self.num_classes, s=64, m=0.5
+            in_features=self.embedding_size,
+            out_features=self.num_classes,
+            **CONFIG["ArcFace"]
         )
         # print("margin", self.margin)
         self.bn1 = nn.BatchNorm2d(self.in_features)
@@ -148,6 +152,56 @@ class HN_NFNET_l0(nn.Module):
         # print("features at view", features.shape)
         features = self.fc1(features)
         # print(features.shape)
+        features = self.bn2(features)
+        features = F.normalize(features)
+        # print(features, features.shape)
+        if labels is not None:
+            MARGIN = self.margin(features, labels)
+            # print("Margin | {} | {}".format(MARGIN, MARGIN.shape))
+            return self.margin(features, labels)
+        return features
+
+
+class TEST_MODEL(nn.Module):
+    def __init__(
+        self,
+        channel_size,
+        out_feature,
+        dropout=0.2,
+        backbone="eca_nfnet_l1",
+        pretrained=True,
+    ):
+        super(TEST_MODEL, self).__init__()
+        self.backbone = timm.create_model(backbone, pretrained=pretrained)
+        self.channel_size = channel_size
+        self.out_feature = out_feature
+        self.in_features = self.backbone.head.fc.in_features
+        print(self.in_features)
+        self.margin = ArcModule(
+            in_features=self.channel_size,
+            out_features=self.out_feature,
+            **CONFIG["ArcFace"]
+        )
+        # print("margin", self.margin)
+        self.bn1 = nn.BatchNorm2d(self.in_features)
+        self.dropout = nn.Dropout2d(dropout, inplace=True)
+        self.fc1 = nn.Linear(self.in_features * 16 * 16, self.channel_size)
+        self.bn2 = nn.BatchNorm1d(self.channel_size)
+
+    def forward(self, x, labels=None):
+
+        features = self.backbone.forward_features(x)
+
+        print("features at backbone", features.shape)
+        features = self.bn1(features)
+        print("after bn1", features.shape)
+        features = self.dropout(features)
+        print("after dropout2d", features.shape)
+        features = features.view(features.size(0), -1)
+
+        print("features at view", features.shape)
+        features = self.fc1(features)
+        print("after fc1", features.shape)
         features = self.bn2(features)
         features = F.normalize(features)
         # print(features, features.shape)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from seed import seed_all
-from model import SHOPEE_HIRE_ME_MODEL
+from model import *
 import torch
 from config import CONFIG
 
@@ -14,14 +14,23 @@ if __name__ == "__main__":
     #     pretrained=True,
     # )
     # model.to(device)
-    model = SHOPEE_HIRE_ME_MODEL(
-        num_classes=CONFIG["NUM_CLASSES"],
-        dropout=CONFIG["MODEL"]["DROPOUT"],
-        embedding_size=CONFIG["MODEL"]["FC_DIM"],
+    # model = SHOPEE_HIRE_ME_MODEL(
+    #     num_classes=CONFIG["NUM_CLASSES"],
+    #     dropout=CONFIG["MODEL"]["DROPOUT"],
+    #     embedding_size=CONFIG["MODEL"]["FC_DIM"],
+    #     backbone="vgg16",
+    #     pretrained=True,
+    # )
+    # model.to(device)
+    torch.cuda.empty_cache()
+    model = TEST_MODEL(
+        channel_size=512,
+        out_feature=11014,
+        dropout=0.0,
         backbone="vgg16",
         pretrained=True,
     )
-    model.to(device)
+    model = model.to(device)
 
     # GOOD Practice to test forward pass, print out sin's inputs shape
     # torch.Size([16, 3, 512, 512]) torch.Size([16])
@@ -35,10 +44,10 @@ if __name__ == "__main__":
     # print("input:\t{}\noutput:\t{}".format(x.shape, y1.shape))
     # print("output value", y1[0][0])
 
-    x = torch.rand(16, 3, 64, 64).to(
+    x = torch.rand(2, 3, 64, 64).to(
         device
     )  # 16 batch size, of 512 size images of 3 channels
-    y = torch.rand(16).to(device)
+    y = torch.rand(2).to(device)
     # with torch.no_grad():
     y1 = model(x, y)
     print("[forward test]")
