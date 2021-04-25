1235bac108416ce7b6f5d46bf7e55abc46d1a570
Hongnan <hongnang.sph@gmail.com>
2021-04-25 16:55:31 +0800

    1

---
+++ ArcMargin.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+class ArcModule(nn.Module):
+    def __init__(
+        self,
+        in_features,
+        out_features,
+        scale=64,
+        margin=0.5,
+        easy_margin=False,
+        ls_eps=0,
+    ):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.scale = scale
+        self.margin = margin
+        self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_normal_(self.weight)
+
+        self.cos_m = math.cos(margin)
+        self.sin_m = math.sin(margin)
+        self.th = torch.tensor(math.cos(math.pi - margin))
+        self.mm = torch.tensor(math.sin(math.pi - margin) * margin)
+
+    def forward(self, inputs, labels):
+        cos_th = F.linear(inputs, F.normalize(self.weight))
+        cos_th = (
+            cos_th.float()
+        )  ###TODO: do not put this below as it will cause numerical instability.
+        cos_th = cos_th.clamp(-1, 1)
+        sin_th = torch.sqrt(1.0 - torch.pow(cos_th, 2))
+        cos_th_m = cos_th * self.cos_m - sin_th * self.sin_m
+
+        cos_th_m = torch.where(cos_th > self.th, cos_th_m, cos_th - self.mm)
+
+        cond_v = cos_th - self.th
+        cond = cond_v <= 0
+        cos_th_m[cond] = (cos_th - self.mm)[cond]
+
+        if labels.dim() == 1:
+            labels = labels.unsqueeze(-1)
+        onehot = torch.zeros(cos_th.size()).cuda()
+        labels = labels.type(torch.LongTensor).cuda()
+        onehot.scatter_(1, labels, 1.0)
+        outputs = onehot * cos_th_m + (1.0 - onehot) * cos_th
+        outputs = outputs * self.scale
+        return outputs
--- model.py
+++ model.py
@@ -103,7 +103,7 @@ class SHOPEE_EfficientNetB4(nn.Module):
         return features
 
 
-class SHOPEE_HIRE_ME_MODEL_IDKSHAPEERROR(nn.Module):
+class SHOPEE_PLEASE_HIRE_US(nn.Module):
     def __init__(
         self,
         num_classes=11014,
@@ -112,151 +112,61 @@ class SHOPEE_HIRE_ME_MODEL_IDKSHAPEERROR(nn.Module):
         backbone="vgg16",
         pretrained=True,
     ):
-        super(SHOPEE_HIRE_ME_MODEL, self).__init__()
-        self.backbone = timm.create_model(backbone, pretrained=pretrained)
+        super(SHOPEE_PLEASE_HIRE_US, self).__init__()
+
         self.embedding_size = embedding_size
         self.num_classes = num_classes
+        self.backbone = timm.create_model(backbone, pretrained=pretrained)
+        self.adaptive_pooling = torch.nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.backbone.reset_classifier(num_classes=0, global_pool="avg")
 
-        self.in_features = self.backbone.head.fc.in_features
-        self.margin = ArcModule(
+        in_features = self.backbone.num_features
+
+        self.BN_DR_FC_BN = torch.nn.Sequential(  # Now since the classifier head is reset, we replace it with                                                                             #
+            torch.nn.BatchNorm1d(
+                self.in_features
+            ),  # BN_DR_FC_BN - the proposed layer architecture by ArcFace.
+            torch.nn.Dropout(
+                p=0.0
+            ),  # Applies Batch Normalization over a 2D or 3D input and since
+            torch.nn.Linear(
+                self.in_features, self.embedding_size
+            ),  # the previous layer output is [16, 4096, 1] we ise BN_1D
+            torch.nn.BatchNorm1d(
+                self.embedding_size
+            ),  # and lastly connect with Dropout, FC and BN again. where
+        )  # fc_dim is the embeddings we want, which is proposed to be 512.
+        self._init_params()
+
+        self.ArcMargin = ArcModule(
             in_features=self.embedding_size,
             out_features=self.num_classes,
             **CONFIG["ArcFace"]
         )
-        # print("margin", self.margin)
-        self.bn1 = nn.BatchNorm2d(self.in_features)
-        self.dropout = nn.Dropout2d(dropout, inplace=True)
-        self.fc1 = nn.Linear(self.in_features, self.embedding_size)
-        self.bn2 = nn.BatchNorm1d(self.embedding_size)
-        print(self.num_classes, self.in_features)
+
+    def _init_params(self):
+        # CUSTOM INIT note that try to think if there is a more
+        # suggestive way to init the layer weights instead of using [1]
+
+        nn.init.constant_(self.BN_DR_FC_BN[0].weight, 1)
+        nn.init.constant_(self.BN_DR_FC_BN[0].bias, 0)
+        nn.init.xavier_normal_(self.BN_DR_FC_BN[2].weight, 1)
+        nn.init.constant_(self.BN_DR_FC_BN[2].bias, 0)
+        nn.init.constant_(self.BN_DR_FC_BN[3].weight, 1)
+        nn.init.constant_(self.BN_DR_FC_BN[3].bias, 0)
 
     def forward(self, x, labels=None):
-        print(x.shape)
-        print(labels.shape)
+        batch_size = x.shape[0]
         features = self.backbone.forward_features(x)
-        features_2 = self.backbone.forward(x)
-        # print(features_2.shape)
-        print(features.shape)
-        # print("features at backbone", features.shape)
-        features = self.bn1(features)
-        print(features.shape)
-        print(features)
-        features = self.dropout(features)
-        features = features.view(features.size(0), -1)
-        print(features.shape)
-
-        # print("features at view", features.shape)
-        features = self.fc1(features)
-        # print(features.shape)
-        features = self.bn2(features)
-        # print(features.shape)
-        features = F.normalize(features)
-        print(features.shape)
-        # print(features, features.shape)
-        if labels is not None:
-            MARGIN = self.margin(features, labels)
-            # print("Margin | {} | {}".format(MARGIN, MARGIN.shape))
-            return self.margin(features, labels)
-        return features
-
-
-class HN_NFNET_l0(nn.Module):
-    def __init__(
-        self,
-        channel_size,
-        out_feature,
-        dropout=0.2,
-        backbone="eca_nfnet_l1",
-        pretrained=True,
-    ):
-        super(HN_NFNET_l0, self).__init__()
-        self.backbone = timm.create_model(backbone, pretrained=pretrained)
-        self.channel_size = channel_size
-        self.out_feature = out_feature
-        self.in_features = self.backbone.head.fc.in_features
-        print(self.in_features)
-        self.margin = ArcModule(
-            in_features=self.channel_size,
-            out_features=self.out_feature,
-            **CONFIG["ArcFace"]
-        )
-        # print("margin", self.margin)
-        self.bn1 = nn.BatchNorm2d(self.in_features)
-        self.dropout = nn.Dropout2d(dropout, inplace=True)
-        self.fc1 = nn.Linear(self.in_features * 16 * 16, self.channel_size)
-        self.bn2 = nn.BatchNorm1d(self.channel_size)
-
-    def forward(self, x, labels=None):
-
-        features = self.backbone.forward_features(x)
-
-        # print("features at backbone", features.shape)
+        features = self.adaptive_pooling(features)
+        features = features.view(batch_size, -1)
         features = self.bn1(features)
         features = self.dropout(features)
-        print(features.shape)
+        features = self.adaptive_pooling(features)
         features = features.view(features.size(0), -1)
-        # print("features at view", features.shape)
-        features = self.fc1(features)
-        # print(features.shape)
-        features = self.bn2(features)
-        features = F.normalize(features)
-        # print(features, features.shape)
+        features = self.BN_DR_FC_BN(features)
+
         if labels is not None:
-            MARGIN = self.margin(features, labels)
-            # print("Margin | {} | {}".format(MARGIN, MARGIN.shape))
-            return self.margin(features, labels)
-        return features
-
-
-class TEST_MODEL(nn.Module):
-    def __init__(
-        self,
-        channel_size,
-        out_feature,
-        dropout=0.2,
-        backbone="eca_nfnet_l1",
-        pretrained=True,
-    ):
-        super(TEST_MODEL, self).__init__()
-        self.backbone = timm.create_model(backbone, pretrained=pretrained)
-        self.channel_size = channel_size
-        self.out_feature = out_feature
-        # self.in_features = self.backbone.head.fc.in_features
-        self.in_features = self.backbone.classifier.in_features
-        print(self.in_features)
-        self.margin = ArcModule(
-            in_features=self.channel_size,
-            out_features=self.out_feature,
-            **CONFIG["ArcFace"]
-        )
-        self.adaptive_avg_pool = torch.nn.AdaptiveAvgPool2d((1, 1))
-        # print("margin", self.margin)
-        self.bn1 = nn.BatchNorm2d(self.in_features)
-        self.dropout = nn.Dropout2d(dropout, inplace=True)
-
-        self.fc1 = nn.Linear(self.in_features, self.channel_size)  # * 16 * 16
-        self.bn2 = nn.BatchNorm1d(self.channel_size)
-
-    def forward(self, x, labels=None):
-
-        features = self.backbone.forward_features(x)
-
-        print("features at backbone", features.shape)
-        features = self.bn1(features)
-        print("after bn1", features.shape)
-        features = self.dropout(features)
-        print("after dropout2d", features.shape)
-        features = self.adaptive_avg_pool(features)
-        features = features.view(features.size(0), -1)
-
-        print("features at view", features.shape)
-        features = self.fc1(features)
-        print("after fc1", features.shape)
-        features = self.bn2(features)
-        features = F.normalize(features)
-        # print(features, features.shape)
-        if labels is not None:
-            MARGIN = self.margin(features, labels)
-            # print("Margin | {} | {}".format(MARGIN, MARGIN.shape))
-            return self.margin(features, labels)
+            arcfaceLogits = self.ArcMargin(features, labels)
+            return arcfaceLogits
         return features
