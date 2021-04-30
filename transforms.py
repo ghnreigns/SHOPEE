5274828a75d60614fbc2eaa6936a8f07cbb6f368
Hongnan <hongnang.sph@gmail.com>
2021-05-01 00:21:49 +0800

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -35,66 +35,62 @@ transforms_valid = albumentations.Compose(
 
 transforms_train = albumentations.Compose(
     [
-        CONFIG["TRAINING"]["IMAGE_SIZE"],
-        CONFIG["TRAINING"]["IMAGE_SIZE"],
-        albumentations.Compose(
+        albumentations.Resize(
+            CONFIG["TRAINING"]["IMAGE_SIZE"], CONFIG["TRAINING"]["IMAGE_SIZE"]
+        ),
+        albumentations.OneOf(
             [
-                albumentations.OneOf(
-                    [
-                        albumentations.GaussNoise(mean=15),
-                        albumentations.MotionBlur(p=0.2),
-                    ]
+                albumentations.GaussNoise(mean=15),
+                albumentations.MotionBlur(p=0.2),
+            ]
+        ),
+        albumentations.OneOf(
+            [
+                albumentations.RGBShift(
+                    p=1.0,
+                    r_shift_limit=(-10, 10),
+                    g_shift_limit=(-10, 10),
+                    b_shift_limit=(-10, 10),
                 ),
-                albumentations.OneOf(
-                    [
-                        albumentations.RGBShift(
-                            p=1.0,
-                            r_shift_limit=(-10, 10),
-                            g_shift_limit=(-10, 10),
-                            b_shift_limit=(-10, 10),
-                        ),
-                        albumentations.RandomBrightnessContrast(
-                            brightness_limit=0.3, contrast_limit=0.1, p=1
-                        ),
-                        albumentations.HueSaturationValue(hue_shift_limit=20, p=1),
-                    ],
-                    p=0.6,
+                albumentations.RandomBrightnessContrast(
+                    brightness_limit=0.3, contrast_limit=0.1, p=1
                 ),
-                albumentations.OneOf(
-                    [
-                        albumentations.CLAHE(clip_limit=2),
-                        albumentations.IAASharpen(),
-                        albumentations.IAAEmboss(),
-                    ]
-                ),
-                albumentations.OneOf(
-                    [
-                        albumentations.IAAPerspective(p=0.3),
-                        albumentations.ElasticTransform(p=0.1),
-                    ]
-                ),
-                albumentations.OneOf(
-                    [
-                        albumentations.Rotate(limit=25, p=0.6),
-                        albumentations.IAAAffine(
-                            scale=0.9,
-                            translate_px=15,
-                            rotate=25,
-                            shear=0.2,
-                        ),
-                    ],
-                    p=1,
-                ),
-                albumentations.Cutout(
-                    num_holes=1,
-                    max_h_size=CONFIG["TRAINING"]["IMAGE_SIZE"] // 5,
-                    max_w_size=CONFIG["TRAINING"]["IMAGE_SIZE"] // 5,
-                    p=0.2,
+                albumentations.HueSaturationValue(hue_shift_limit=20, p=1),
+            ],
+            p=0.6,
+        ),
+        albumentations.OneOf(
+            [
+                albumentations.CLAHE(clip_limit=2),
+                albumentations.IAASharpen(),
+                albumentations.IAAEmboss(),
+            ]
+        ),
+        albumentations.OneOf(
+            [
+                albumentations.IAAPerspective(p=0.3),
+                albumentations.ElasticTransform(p=0.1),
+            ]
+        ),
+        albumentations.OneOf(
+            [
+                albumentations.Rotate(limit=25, p=0.6),
+                albumentations.IAAAffine(
+                    scale=0.9,
+                    translate_px=15,
+                    rotate=25,
+                    shear=0.2,
                 ),
             ],
             p=1,
         ),
-        albumentations.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        albumentations.Cutout(
+            num_holes=1,
+            max_h_size=CONFIG["TRAINING"]["IMAGE_SIZE"] // 5,
+            max_w_size=CONFIG["TRAINING"]["IMAGE_SIZE"] // 5,
+            p=0.2,
+        ),
+        albumentations.Normalize(),
         ToTensorV2(p=1.0),
     ]
 )
