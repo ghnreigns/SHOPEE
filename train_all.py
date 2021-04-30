593bbcbc557b75ddcbf1cc68e83e2a0aea3bb1fa
Hongnan <hongnang.sph@gmail.com>
2021-05-01 07:52:48 +0800

    Update train_all.py

--- train_all.py
+++ train_all.py
@@ -15,7 +15,8 @@ from model import *
 from model_HN import *
 
 # from config import CONFIG
-from config_eca_nfnet_l1 import CONFIG
+# from config_eca_nfnet_l1 import CONFIG
+from config_efficientnet import CONFIG
 from threshold import find_threshold
 
 
