50a5bf60ef8cdb06f54b4ef70e90eeba04927bbd
Hongnan <hongnang.sph@gmail.com>
2021-05-01 00:14:25 +0800

    1

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ CONFIG = {
     "TRAINING": {
         "IMAGE_SIZE": 512,
         "BATCH_SIZE": 16,
-        "NUM_EPOCHS": 12,
+        "NUM_EPOCHS": 16,
         "USE_AMP": True,
         "NUM_WORKERS": 4,
         "ACCUMULATION_STEP": 1,
@@ -19,14 +19,16 @@ CONFIG = {
     "VALIDATION": {"BATCH_SIZE": 16, "NUM_WORKERS": 4},
     "OPTIMIZER": {"Adam": {"lr": 1e-4}},
     "SCHEDULER": {
-        "CosineAnnealingWarmRestarts": {"T_0": 12, "T_mult": 2, "eta_min": 0}
+        "CosineAnnealingLR": {"T_max": 16, "eta_min": 1e-7, "last_epoch": -1}
+        # "CosineAnnealingWarmRestarts": {"T_0": 12, "T_mult": 2, "eta_min": 0}
     },
     "ArcFace": {"scale": 30, "margin": 0.5, "easy_margin": False, "ls_eps": 0.0},
     "AUGMENTATION": {},
     "PATH": {
         "TRAINING_CSV": "/content/train.csv",
         "TRAIN_PATH": "/content/train_images",
-        "SAVE_WEIGHT_PATH": "/content/drive/My Drive/Shopee - Price Match Guarantee/WEIGHTS/eca_nfnet_l0/30_APRIL_2021_V2/",
+        "SAVE_WEIGHT_PATH": "/content/drive/My Drive/Shopee - Price Match Guarantee/WEIGHTS/efficientnetb6ns/30_APRIL_2021/"
+        # "SAVE_WEIGHT_PATH": "/content/drive/My Drive/Shopee - Price Match Guarantee/WEIGHTS/eca_nfnet_l0/30_APRIL_2021_V2/",
     },
     "FOLD": "ALL",
     "SEARCH_SPACE": np.arange(40, 100, 10),
